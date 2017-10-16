23e94b2aed253eebbf40512132b288cd63f69485
Anthony Gonzalez <anthonyg9942@gmail.com>
2017-10-16 13:49:07 -0400

    idk lel

--- main.cpp
+++ main.cpp
@@ -8,23 +8,43 @@
  * File:   main.cpp
  * Author: agonzalez
  *
- * Created on October 11, 2017, 12:49 PM
+ * Created on October 11, 20817, 12:49 PM
  */
 
 #include <cstdlib>
 
 using namespace std;
 class Base{
+public: 
+    Base(){};
+    void draw(string drawing){
+        cout << drawing << endl;
+    }
+};
+class draw1: public Base{
+public:
+    Draw(){
+        Base::draw("Drawing 1:");
+        Base::draw(" /\\ ");
+        Base::draw(" //\\\\ ");
+    }
     
 };
-class draw1{
-    
+class draw2 :public Base{
+public:
+    Draw(){
+        Base::draw("Drawing 2:");
+        Base::draw(" /\\ ");
+        Base::draw(" /**\\ ");
+    }
 };
-class draw2{
-    
-};
-class draw3{
-    
+class draw3:public Base{
+public:
+    Draw(){
+        Base::draw("Drawing 2:");
+        Base::draw(" /\\ ");
+        Base::draw(" /++\\ ");
+    }
 };
 /*
  * 

a7ece2e769e40001dbc2eed6332607a9a96c8dcd
Anthony Gonzalez <anthonyg9942@gmail.com>
2017-10-17 14:08:10 -0400

    idk lel

--- main.cpp
+++ main.cpp
@@ -18,36 +18,33 @@ using namespace std;
 class Base{
 public: 
     Base(){};
-    string draw(string drawing){
-        cout << drawing << endl;
+    virtual void getDraw(){
+        cout << 'Test lol' << endl;
     }
 };
 class draw1: public Base{
 public:
     
-        void getDraw(void){
-        Base::draw("Drawing 1:");
-        Base::draw(" /\\ ");
-        Base::draw("//\\\\");
+        void getDraw(){
+        cout << "Drawing 1:" << endl << " /\\ " << endl << "//\\\\" << endl;
+      
         }
 };
 class draw2: public Base{
 public:
-    
-        void getDraw(void){
-        Base::draw("Drawing 1:");
-        Base::draw(" /\\ ");
-        Base::draw("//\\\\");
+    void getDraw(){
+        cout << "Drawing 2:" << endl << " /\\ " << endl << "/**\\" << endl;
+      
         }
+        
 };
 class draw3: public Base{
 public:
-   
-    void getDraw(void){
-    Base::draw("Drawing 1:");
-    Base::draw(" /\\ ");
-    Base::draw("//\\\\");
+   void getDraw(){
+        cout << "Drawing 3:" << endl << " /\\ " << endl << "/++\\" << endl;
+      
         }
+    
 };
 /*
  * 
@@ -56,12 +53,21 @@ int main(int argc, char** argv) {
     draw1 *Draw1;
     draw2 *Draw2;
     draw3 *Draw3;
+
+    Draw1 = new draw1;
+    Draw2 = new draw2;
+    Draw3 = new draw3;
     
-    Draw1 = new draw1();
-    Draw2 = new draw2();
-    Draw3 = new draw3();
+    Base *array[3];
     
+    array[0]= new draw1();
+    array[1]= new draw2();
+    array[2]= new draw3();
     
+    for(int i = 0; i < sizeof(array); i++)
+    {
+        array[i]->getDraw();
+    }
     return 0;
 }
 
